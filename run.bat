ab58e19d9c523a44c37efae0ad2e34f33dfb7f93
KARRI LAKSHMI SUREKHA <lakshmisurekha226@gmail.com>
2026-03-06 09:42:25 +0530

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Compiling Bus Ticket Booking System...
-javac -cp "lib\mysql-connector-j-9.6.0.jar;." src\*.java -d .
-
-echo Running application...
-java -cp "lib\mysql-connector-j-9.6.0.jar;." Main
-
-pause
